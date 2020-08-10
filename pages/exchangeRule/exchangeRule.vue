f7dcba2baf1cf01e2914268451bdc63b7c1a0688
cuishuyuan <17831135193@163.com>
2020-08-10 17:58:24 +0800

    管理公司积分兑换项目uniapp版

---
+++ pages/exchangeRule/exchangeRule.vue
@@ -0,0 +1,22 @@
+<template>
+	<view id="exchangeRuleWrap">
+		兑换规则页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view id="indexContainer">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="stylus">
+	
+</style>
